ce8d25d31cef19a6dc0dcb0d273a3da5a3df8b13
SujayVivek <sujayv2016@gmail.com>
2025-12-18 21:43:29 +0530

    2

---
+++ solved/A_Operations_with_Inversions.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define ff first
+#define ss second
+typedef vector<vector<long long>> vvi;
+typedef vector<long long> vi;
+#define int long long
+#define endl "\n"
+
+void Solve() {
+    int n; cin>>n;
+    int mx = -1;
+    int cnt = 0;
+    for(int i = 0; i<n; i++){
+        int x; cin>>x;
+        if(x<mx) cnt++;
+        else mx = x;
+    }
+    cout<<cnt<<endl;
+}
+
+int32_t main() {
+    int tt_ = 1;
+    cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
+    return 0;
+}
