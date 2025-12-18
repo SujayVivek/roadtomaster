5610fcdaf6c59cbd5e1cdc5fea46ecfeda09ccde
SujayVivek <sujayv2016@gmail.com>
2025-12-18 21:43:34 +0530

    4

---
+++ solved/A_Suspension.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define ff first
+#define ss second
+typedef vector<vector<long long>> vvi;
+typedef vector<long long> vi;
+#define int long long
+#define endl "\n"
+
+void Solve() {
+    int n; cin>>n;
+    int y, r; cin>>y>>r;
+    int ans = min(r, n);
+    n-=ans;
+    ans+= min(y/2, n);
+    cout<<ans<<endl;
+}
+
+int32_t main() {
+    int tt_ = 1;
+    cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
+    return 0;
+}
