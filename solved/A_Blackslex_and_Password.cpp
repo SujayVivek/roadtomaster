8bac924be185d7719efc8b5a766dc0410470f070
SujayVivek <sujayv2016@gmail.com>
2025-12-27 11:47:05 +0530

    1

---
+++ solved/A_Blackslex_and_Password.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define ff first
+#define ss second
+typedef vector<vector<long long>> vvi;
+typedef vector<long long> vi;
+#define int long long
+#define endl "\n"
+
+void Solve() {
+    int k, x; cin>>k>>x;
+    int pos = 1, cnt = 1;
+    while(pos<k){
+        pos++;
+        cnt+=x;
+    }
+    cout<<cnt+x<<endl;
+}
+
+int32_t main() {
+    int tt_ = 1;
+    cin >> tt_;
+    while (tt_--) {
+        Solve();
+    }
+    return 0;
+}
